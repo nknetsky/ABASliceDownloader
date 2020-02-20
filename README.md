21c87be4197469097f491eca466be06e68299a5b
Erik Liu <nknetsky@gmail.com>
2020-02-20 09:59:55 +0900

    add license and README comments in main.py

---
+++ README.md
@@ -0,0 +1,32 @@
+# ABASliceDownloader
+
+Automatically download a full set of ISH images of a gene
+
+-   Latest modified date: February 20, 2020
+-   Latest version: v0.1.0
+-   First established date: February 20, 2020
+
+Goals of development:
+
+-   use python argparse package
+-   can detect empty results of a search
+-   can specify downloading saggital or coronal images
+
+### Command
+
+
+
+
+### Required packages
+
+-   allensdk
+-   selenium
+-   chromedriver
+
+### License
+
+MIT
+
+### Changelog
+
+-   v0.1.1
--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import sys
 # %%
 path = "/Users/erikliu/Desktop/"
 
-# '-s' indicat search for the experiment using the input name
+# '-s' followed by a keyword
+# search for the experiment using the keyword
 if "-s" in sys.argv:
     # use the keyword after '-s' for the search
     try:
@@ -17,15 +18,23 @@ if "-s" in sys.argv:
     except ValueError:
         print("Place an input name after -s.")
 
+# '-p' followed by a path
+# specify the path to store the images
 if "-p" in sys.argv:
     try:
         path = sys.argv[sys.argv.index("-p") + 1]
     except ValueError:
         print("Place an input name after -p.")
 
+# '-d' followed by an experiment id
+# download the images of the experiment id
 if "-d" in sys.argv:
     exp_id = sys.argv[sys.argv.index("-d") + 1]
     gene_name = get_gene_by_id(results_df, exp_id)
+    
+    # '-dir' followed by a directory name
+    # specify the directory name to store the images
+    # if not specified, the gene name of the experiment id is used
     if "--dir" in sys.argv:
         try:
             dirname = sys.argv[sys.argv.index("--dir") + 1]
